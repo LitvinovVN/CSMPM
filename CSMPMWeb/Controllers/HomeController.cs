b3a6fc18f6e91bbc8c003c61fc515aa190dfaf84
LitvinovVN <LitvinovVN@rambler.ru>
2019-07-11 22:22:25 +0300

    update.sh start

--- CSMPMWeb/Controllers/HomeController.cs
+++ CSMPMWeb/Controllers/HomeController.cs
@@ -20,6 +20,28 @@ namespace CSMPMWeb.Controllers
             return View();
         }
 
+        public IActionResult SystemUpdate()
+        {
+            var process = new Process();
+            process.EnableRaisingEvents = true; // to avoid [defunct] sh processes
+            process.StartInfo.FileName = "/var/CSMPM/update.sh";
+            process.StartInfo.Arguments = "";
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit(10000);
+            if (process.HasExited)
+            {
+                Console.WriteLine("Exit code: " + process.ExitCode);
+            }
+            else
+            {
+                Console.WriteLine("Child process still running after 10 seconds");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
