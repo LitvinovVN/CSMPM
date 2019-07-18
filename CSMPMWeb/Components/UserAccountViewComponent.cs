762531a0760f20ef7d9b774584708453d14c5450
LitvinovVN <LitvinovVN@rambler.ru>
2019-07-18 10:33:40 +0300

    login

---
+++ CSMPMWeb/Components/UserAccountViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSMPMWeb.Components
+{
+    public class UserAccountViewComponent : ViewComponent
+    {
+        public UserAccountViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            string userName = User.Identity.Name;
+
+            return View((object)userName);
+        }
+    }
+}
---
+++ CSMPMWeb/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using CSMPMWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CSMPMWeb.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signInMagr)
+        {
+            userManager = userMgr;
+            signInManager = signInMagr;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            var identity1 = User.Identity;
+
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    var identity2 = User.Identity;
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Неверные учетные данные");
+            }
+
+            return View(details);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
